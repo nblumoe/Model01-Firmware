8899ec146eb1862239f47c2fce0e5659aaa9674e
Nils Blum-Oeste <nils@blum-oeste.de>
2019-02-13 15:47:25 +0100

    Clean up commented code

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -7,13 +7,6 @@
 #endif
 
 
-/**
- * These #include directives pull in the Kaleidoscope firmware core,
- * as well as the Kaleidoscope plugins we use in the Model 01's firmware
- */
-
-
-// The Kaleidoscope core
 #include "Kaleidoscope.h"
 
 // Support for storing the keymap in EEPROM
@@ -23,41 +16,17 @@
 // Support for communicating with the host via a simple Serial protocol
 #include "Kaleidoscope-FocusSerial.h"
 
-// Support for keys that move the mouse
 #include "Kaleidoscope-MouseKeys.h"
-
-// Support for macros
 #include "Kaleidoscope-Macros.h"
-
-// Support for controlling the keyboard's LEDs
 #include "Kaleidoscope-LEDControl.h"
-
-// Support for "Numpad" mode, which is mostly just the Numpad specific LED mode
-// #include "Kaleidoscope-NumPad.h"
 
 // Support for the "Boot greeting" effect, which pulses the 'LED' button for 10s
 // when the keyboard is connected to a computer (or that computer is powered on)
 #include "Kaleidoscope-LEDEffect-BootGreeting.h"
 
-// Support for LED modes that set all LEDs to a single color
 #include "Kaleidoscope-LEDEffect-SolidColor.h"
-
-// Support for an LED mode that makes all the LEDs 'breathe'
 #include "Kaleidoscope-LEDEffect-Breathe.h"
-
-// Support for an LED mode that makes a red pixel chase a blue pixel across the keyboard
-// #include "Kaleidoscope-LEDEffect-Chase.h"
-
-// Support for LED modes that pulse the keyboard's LED in a rainbow pattern
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
-
-// Support for an LED mode that lights up the keys as you press them
-#include "Kaleidoscope-LED-Stalker.h"
-
-// Support for an LED mode that prints the keys you press in letters 4px high
-// #include "Kaleidoscope-LED-AlphaSquare.h"
-
-// The LEDs under each key will have a color according to how much use they see.
 #include <Kaleidoscope-Heatmap.h>
 
 // Support for Keyboardio's internal keyboard testing mode
@@ -66,16 +35,9 @@
 // Support for host power management (suspend & wakeup)
 #include "Kaleidoscope-HostPowerManagement.h"
 
-// Support for magic combos (key chords that trigger an action)
-// #include "Kaleidoscope-MagicCombo.h"
-
 // Support for USB quirks, like changing the key state report protocol
 #include "Kaleidoscope-USB-Quirks.h"
 
-// SpaceCadet plugin to type parens easily
-// #include <Kaleidoscope-SpaceCadet.h>
-
-//#include <Kaleidoscope-Qukeys.h>
 
 /** This 'enum' is a list of all the macros used by the Model 01's firmware
   * The names aren't particularly important. What is important is that each
@@ -144,21 +106,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { PRIMARY, NUMPAD, FUNCTION }; // layers
-
-
-/**
-  * To change your keyboard's layout from QWERTY to DVORAK or COLEMAK, comment out the line
-  *
-  * #define PRIMARY_KEYMAP_QWERTY
-  *
-  * by changing it to
-  *
-  * // #define PRIMARY_KEYMAP_QWERTY
-  *
-  * Then uncomment the line corresponding to the layout you want to use.
-  *
-  */
+enum { PRIMARY, NUMPAD, FUNCTION };
 
 #define PRIMARY_KEYMAP_QWERTY
 // #define PRIMARY_KEYMAP_COLEMAK
@@ -180,8 +128,6 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   // Qukey mappings
-//   Key_LeftCurlyBracket, Key_Backspace[, Key_LeftBracket, Key_LeftParen,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
@@ -189,8 +135,6 @@ KEYMAPS(
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightGui,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   // Qukey mappings
-//   Key_RightParen, Key_RightBracket, Key_Spacebar, Key_RightCurlyBracket,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
@@ -229,7 +173,6 @@ KEYMAPS(
    ShiftToLayer(FUNCTION)),
 
 #elif defined (PRIMARY_KEYMAP_CUSTOM)
-  // Edit this keymap to make a custom layout
   [PRIMARY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
@@ -418,17 +361,8 @@ static const cRGB heat_colors[] PROGMEM = {
   { 25,  25, 255}  // red
 };
 
-/** Magic combo list, a list of key combo and action pairs the firmware should
- * recognise.
- */
-//USE_MAGIC_COMBOS({.action = toggleKeyboardProtocol,
-//                  // Left Fn + Esc + Shift
-//                  .keys = { R3C6, R2C6, R3C7 }
-//                 });
 
-// First, tell Kaleidoscope which plugins you want to use.
-// The order can be important. For example, LED effects are
-// added in the order they're listed here.
+
 KALEIDOSCOPE_INIT_PLUGINS(
   // The EEPROMSettings & EEPROMKeymap plugins make it possible to have an
   // editable keymap in EEPROM.
@@ -479,36 +413,15 @@ KALEIDOSCOPE_INIT_PLUGINS(
   // The breathe effect slowly pulses all of the LEDs on your keyboard
   LEDBreatheEffect,
 
-  // The AlphaSquare effect prints each character you type, using your
-  // keyboard's LEDs as a display
-  // AlphaSquareEffect,
-
-  // The stalker effect lights up the keys you've pressed recently
-  StalkerEffect,
-
   // effect that turns off all the LEDs.
   LEDOff,
-
-//  Qukeys,
-
-  // The numpad plugin is responsible for lighting up the 'numpad' mode
-  // with a custom LED effect
-//  NumPad,
 
   // The macros plugin adds support for macros
   Macros,
 
-  // The MouseKeys plugin lets you add keys to your keymap which move the mouse.
-  MouseKeys,
-
   // The HostPowerManagement plugin allows us to turn LEDs off when then host
   // goes to sleep, and resume them when it wakes up.
   HostPowerManagement,
-
-  // The MagicCombo plugin lets you use key combinations to trigger custom
-  // actions - a bit like Macros, but triggered by pressing multiple keys at the
-  // same time.
-//  MagicCombo,
 
   // The USBQuirks plugin lets you do some things with USB that we aren't
   // comfortable - or able - to do automatically, but can be useful
@@ -516,10 +429,8 @@ KALEIDOSCOPE_INIT_PLUGINS(
   // by BIOSes) and Report (NKRO).
   USBQuirks
 
-  //SpaceCadet
 );
 
-//const int spaceCadetTimeout = 250;
 
 /** The 'setup' function is one of the two standard Arduino sketch functions.
  * It's called when your keyboard first powers up. This is where you set up
@@ -529,22 +440,10 @@ void setup() {
   // First, call Kaleidoscope's internal setup function
   Kaleidoscope.setup();
 
-  // While we hope to improve this in the future, the NumPad plugin
-  // needs to be explicitly told which keymap layer is your numpad layer
-//  NumPad.numPadLayer = NUMPAD;
-
-  // We configure the AlphaSquare effect to use RED letters
-//  AlphaSquare.color = CRGB(255, 0, 0);
-
   // We set the brightness of the rainbow effects to 150 (on a scale of 0-255)
   // This draws more than 500mA, but looks much nicer than a dimmer effect
   LEDRainbowEffect.brightness(150);
   LEDRainbowWaveEffect.brightness(150);
-
-  // The LED Stalker mode has a few effects. The one we like is called
-  // 'BlazingTrail'. For details on other options, see
-  // https://github.com/keyboardio/Kaleidoscope/blob/master/doc/plugin/LED-Stalker.md
-  StalkerEffect.variant = STALKER(BlazingTrail);
 
   // We want to make sure that the firmware starts with LED effects off
   // This avoids over-taxing devices that don't have a lot of power to share
@@ -557,46 +456,9 @@ void setup() {
   // by using the `settings.defaultLayer` Focus command.
   EEPROMKeymap.setup(5, EEPROMKeymap.Mode::EXTEND);
 
-//  //Set the keymap with timeout per-key
-//  //Setting is {KeyThatWasPressed, AlternativeKeyToSend, TimeoutInMS}
-//  //Note: must end with the SPACECADET_MAP_END delimiter
-//  static kaleidoscope::SpaceCadet::KeyBinding spacecadetmap[] = {
-//    {Key_LeftShift, Key_LeftParen, spaceCadetTimeout}
-//    , {Key_RightShift, Key_RightParen, spaceCadetTimeout}
-//
-//    , {Key_LeftGui, Key_LeftBracket, spaceCadetTimeout}
-//    , {Key_RightAlt, Key_RightBracket, spaceCadetTimeout}
-//    , {Key_LeftAlt, Key_RightBracket, spaceCadetTimeout}
-//
-//    , {Key_LeftControl, Key_LeftCurlyBracket, spaceCadetTimeout}
-//    , {Key_RightControl, Key_RightCurlyBracket, spaceCadetTimeout}
-//    , SPACECADET_MAP_END
-//  };
-//  //Set the map.
-//  SpaceCadet.map = spacecadetmap;
-
-//  QUKEYS(
-//    kaleidoscope::Qukey(0, 0, 7, Key_LeftControl),
-//    kaleidoscope::Qukey(0, 2, 7, Key_LeftGui),
-//    kaleidoscope::Qukey(0, 3, 7, Key_LeftShift),
-//
-//    kaleidoscope::Qukey(0, 3, 8, Key_RightShift),
-//    kaleidoscope::Qukey(0, 2, 8, Key_RightAlt),
-//    kaleidoscope::Qukey(0, 0, 8, Key_RightControl)
-//  )
-//  Qukeys.setTimeout(200);
-//  Qukeys.setReleaseDelay(20);
-
   HeatmapEffect.heat_colors = heat_colors;
   HeatmapEffect.heat_colors_length = 4;
 }
-
-/** loop is the second of the standard Arduino sketch functions.
-  * As you might expect, it runs in a loop, never exiting.
-  *
-  * For Kaleidoscope-based keyboard firmware, you usually just want to
-  * call Kaleidoscope.loop(); and not do anything custom here.
-  */
 
 void loop() {
   Kaleidoscope.loop();
